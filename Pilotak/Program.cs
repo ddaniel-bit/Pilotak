2c17c5980fd7936fb1b2ad8b63119f4b9fcea203
ddaniel <danko7119@gmail.com>
2023-05-19 10:00:24 +0200

    commit

--- Pilotak/Program.cs
+++ Pilotak/Program.cs
@@ -1,7 +1,19 @@
-﻿internal class Program
+﻿using PilotakOsztaly;
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        List<Pilotak> PilotaLista = new List<Pilotak>();
+        StreamReader sr = new StreamReader("pilotak.csv");
+        sr.ReadLine();
+
+        while (!sr.EndOfStream)
+        {
+            string[] pilota = sr.ReadLine().Split(";");
+            PilotaLista.Add(new Pilotak(pilota[0], pilota[1], pilota[2], int.Parse(pilota[3])));
+        }
+        sr.Close();
+
+        Console.WriteLine($"3.feladat: {PilotaLista.Count()}");
     }
 }
