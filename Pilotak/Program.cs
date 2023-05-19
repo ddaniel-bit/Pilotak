e08c4b4c42c05af4142553befec745e77cdfb7be
ddaniel <danko7119@gmail.com>
2023-05-19 10:35:46 +0200

    kopaszodas ellen

--- Pilotak/Program.cs
+++ Pilotak/Program.cs
@@ -10,10 +10,33 @@ internal class Program
         while (!sr.EndOfStream)
         {
             string[] pilota = sr.ReadLine().Split(";");
-            PilotaLista.Add(new Pilotak(pilota[0], pilota[1], pilota[2], int.Parse(pilota[3])));
+            if (pilota[3] == "")
+            {
+                PilotaLista.Add(new Pilotak(pilota[0], pilota[1], pilota[2], 0));
+            }
+            else
+            {
+                PilotaLista.Add(new Pilotak(pilota[0], pilota[1], pilota[2], int.Parse(pilota[3])));
+            }
+
         }
         sr.Close();
 
         Console.WriteLine($"3.feladat: {PilotaLista.Count()}");
+        Console.WriteLine($"4.feladat: {PilotaLista.Last().Nev}");
+        Console.WriteLine("5.feladat:");
+        for (int pilotaindex = 0; pilotaindex < PilotaLista.Count(); pilotaindex++)
+        {
+            string[] ev_ho_nap = PilotaLista[pilotaindex].Szuletesidatum.Split(".");
+            int ev = int.Parse(ev_ho_nap[0]);
+            if (1901 > ev)
+            {
+                Console.WriteLine($"\t{PilotaLista[pilotaindex].Nev} ({PilotaLista[pilotaindex].Szuletesidatum})");
+            }
+        }
+        while (true)
+        {
+
+        }
     }
 }
